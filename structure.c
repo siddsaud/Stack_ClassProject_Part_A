40cec15899918f4771a5bb0a1fd94be75f601727
groverb <bhavaygrover64@gmail.com>
2018-07-03 12:11:44 -0400

    pointer return

--- structure.c
+++ structure.c
@@ -22,7 +22,7 @@ void printAll(student_t*);
 void exitStack();
 void instructions();
 void printTop(student_t*);
-void printStudent(student_t);
+void printStudent(student_t*);
 
 int main(){
     // Bhavay
@@ -55,7 +55,7 @@ void printTop(student_t* top){
         printf("empty stack");
         return;
     }
-    printStudent(*top);
+    printStudent(top);
     
 
 }
@@ -74,11 +74,23 @@ bool checkUniqueID(int id){
 
 void printAll(student_t* top){
     // Bhavay
+    student_t* current=top;
+    while(current->next!=NULL){
+        printStudent(current);
+        current=current->next;
+    }
 }
 
-void exitStack(){
+void exitStack(student_t* top){
+    student_t* current=top;
+    while(current->next!=NULL){
+        pop(current);
+    }
+    free(current);
     // Bhavay
-    
+    printf("Exites the application");
+    exit(0);
+
 }
 
 
@@ -86,7 +98,7 @@ void instructions() {
 	printf("Enter choice:\n1 Top\n2 Push\n3 Pop\n4 Print \n5 Exit Stack Application");
 }
 
-void printStudent(student_t student){
+void printStudent(student_t* student){
     printf("\nid: %d\tname: %s\tGrad. Year: %d\t GPA: %lf\tProgram Name: %s\tSchool Name: %s\n", 
-    student.id, student.name, student.graduatingYear, student.gpa,student.programName, student.schoolName);
+    student->id, student->name, student->graduatingYear, student->gpa,student->programName, student->schoolName);
 }

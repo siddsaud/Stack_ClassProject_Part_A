fe72024e7ca0c58cc8f6a5f215240f44200c20e2
Nathaniel Primo <nathaniel.primo@gmail.com>
2018-07-03 13:59:58 -0400

    Added documentation

--- structure.c
+++ structure.c
@@ -1,44 +1,64 @@
+/*
+ * * Group Members:
+ * *    - James Grau
+ * *    - Bhavay Grover
+ * *    - Nathaniel Primo
+ * *    - Saud Siddiqui
+ * * File Name: Stack_ClassProject_Part_A/structure.c
+ * * Assignment: Final_Project
+ * * Creation date: 7/3/2018
+ * * Last Modified: 7/3/2018
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 
 // Initialize a student structure
 typedef struct node {
-// Declare the needed structure items
+
+    // Declare the needed structure items
     int id;
     char *name;
     char *schoolName;
     char *programName;
     int graduatingYear;
     float gpa;
-    struct node* next;
+    struct node *next;
 } student_t;
 
 // prototypes
-student_t* pop(student_t*);
-student_t* push(student_t*);
-bool checkUniqueID(student_t*, int);
-void printAll(student_t*);
-void exitStack(student_t* top);
-void instructions();
-void printTop(student_t*);
-void printStudent(student_t*);
+student_t *pop(student_t *);
 
-int main(){
+student_t *push(student_t *);
+
+bool checkUniqueID(student_t *, int);
+
+void printAll(student_t *);
+
+void exitStack(student_t *top);
+
+void instructions();
+
+void printTop(student_t *);
+
+void printStudent(student_t *);
+
+int main() {
     // Bhavay
     instructions();
-    student_t* head=NULL;
+    student_t *head = NULL;
     int choice;
-    while(choice!=5){
-        switch(choice){
+    while (choice != 5) {
+        switch (choice) {
             case 1:
                 printTop(head);
                 break;
             case 2:
-                head=push(head);
+                head = push(head);
                 break;
             case 3:
-                head=pop(head);
+                head = pop(head);
                 break;
             case 4:
                 printAll(head);
@@ -50,17 +70,22 @@ int main(){
     }
 }
 
-void printTop(student_t* top){
-    if(top==NULL){
+/// Prints the top-most node in the linked-list
+/// \param top The first node of the linked-list
+void printTop(student_t *top) {
+
+    if (top == NULL) {
         printf("empty stack");
         return;
     }
     printStudent(top);
-    
-
 }
 
-student_t* pop(student_t* top){
+/// Removes and prints the top-most node in the linked-list
+/// \param top The first node of the linked-list
+/// \return The current top node of the linked-list
+/// \author Nathaniel
+student_t *pop(student_t *top) {
 
     if (top == NULL) {
 
@@ -69,7 +94,7 @@ student_t* pop(student_t* top){
 
     printStudent(top);
 
-    student_t* popped = top;
+    student_t *popped = top;
 
     top = top->next;
 
@@ -78,34 +103,43 @@ student_t* pop(student_t* top){
     return top;
 }
 
-student_t* push(student_t* top){
+student_t *push(student_t *top) {
     // saud
 }
 
-bool checkUniqueID(student_t* head, int id){
+/// Checks if a given id already exist in the given stack
+/// \param head The first node of the linked-list
+/// \param id The desired id to check against
+/// \return If the given id was not present in the stack
+bool checkUniqueID(student_t *head, int id) {
 
-    student_t* current=head;
-    while(current->next!=NULL){
-        if(head->id == id){
+    student_t *current = head;
+    while (current->next != NULL) {
+        if (head->id == id) {
             return false;
         }
-        current=current->next;
+        current = current->next;
     }
     return true;
 }
 
-void printAll(student_t* top){
-    // Bhavay
-    student_t* current=top;
-    while(current->next!=NULL){
+/// Prints each student in a giving linked-list
+/// \param top The first node of the linked-list
+/// \author Bhavay
+void printAll(student_t *top) {
+
+    student_t *current = top;
+    while (current->next != NULL) {
         printStudent(current);
-        current=current->next;
+        current = current->next;
     }
 }
 
-void exitStack(student_t* top){
-    student_t* current=top;
-    while(current->next!=NULL){
+/// Cleans the linked-list and exits the the programs
+/// \param top
+void exitStack(student_t *top) {
+    student_t *current = top;
+    while (current->next != NULL) {
         pop(current);
     }
     free(current);
@@ -115,12 +149,15 @@ void exitStack(student_t* top){
 
 }
 
-
+/// Prints the current user choices
 void instructions() {
-	printf("Enter choice:\n1 Top\n2 Push\n3 Pop\n4 Print \n5 Exit Stack Application");
+    printf("Enter choice:\n1 Top\n2 Push\n3 Pop\n4 Print \n5 Exit Stack Application");
 }
 
-void printStudent(student_t* student){
-    printf("\nid: %d\tname: %s\tGrad. Year: %d\t GPA: %lf\tProgram Name: %s\tSchool Name: %s\n", 
-    student->id, student->name, student->graduatingYear, student->gpa,student->programName, student->schoolName);
+/// Prints a single student
+/// \param student The desired student to be printed
+void printStudent(student_t *student) {
+    printf("\nid: %d\tname: %s\tGrad. Year: %d\t GPA: %lf\tProgram Name: %s\tSchool Name: %s\n",
+           student->id, student->name, student->graduatingYear, student->gpa, student->programName,
+           student->schoolName);
 }
